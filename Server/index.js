ad08326d85b9be72a6ab10e89a953d059a575040
Wesley Nash <bruticus32@gmail.com>
2022-07-30 15:23:08 -0700

    updated-capstone

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,5 @@
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const {SERVER_PORT} = process.env
-const {seed} = require('./seed.js')
 
 const app = express();
 
