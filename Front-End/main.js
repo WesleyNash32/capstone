a77117d0b6a83d1f4fa7429ffb00a02d5d9d9942
Wesley Nash <bruticus32@gmail.com>
2022-07-29 23:32:34 -0700

    saving-capstone

---
+++ Front-End/main.js
@@ -0,0 +1,6 @@
+const addbtn = document.getElementById('add-dino')
+
+function addDinosaur() {
+    const createDinoCard = document.createElement('div')
+    createDinoCard.classList.add()
+}
---
+++ Server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(4040, () => console.log('Ported at 4040'))
